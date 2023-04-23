0e7832ca8065ed4f33601190a3e2a31e9e685b1a
ryuzaki <ryuzaki>
2023-04-23 14:18:46 +0530

    guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use std::{cmp::Ordering, io};
+
+use rand::Rng;
+
+use colored::*;
+
 fn main() {
-    println!("hello worlds");
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("secret number was {}", secret_number);
+
+    loop {
+        println!("guess a number");
+        let mut guess_input = String::new();
+        io::stdin()
+            .read_line(&mut guess_input)
+            .expect("Failed to read line");
+
+        // let guessed_number: u32 = guess_input.trim().parse().expect("enter a valid number");
+        let guessed_number: u32 = match guess_input.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("{}", "enter a valid number".red());
+                continue;
+            }
+        };
+
+        match guessed_number.cmp(&secret_number) {
+            Ordering::Less => println!("{}", "too small".red()),
+            Ordering::Greater => println!("{}", "too big".red()),
+            Ordering::Equal => {
+                println!("{}", "you win".green());
+                break;
+            }
+        }
+    }
 }
